3bdcb9f13c544987923f6732ac02acedc38799b2
Joshua Joshua George <76731561+joshuahey@users.noreply.github.com>
2022-01-03 00:51:01 -0700

    Delete CMPUT 175 directory

--- CMPUT 175/queues.py
+++
@@ -1,7 +0,0 @@
-try:
-    print("Raising an exception")
-    raise Exception('CMPUT', '175')
-except Exception as inst: # the exception instance
-    print(inst.args) # arguments stored in .args
-    x, y = inst.args # unpack args
-    print('x =', x,'y =', y)
